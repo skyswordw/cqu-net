8435ebb9641662625b64f0121a984b55d5ca8ff7
skywalker <lovestarsni@gmail.com>
2025-01-17 21:15:20 +0800

    Refactor cqu_net.sh, docker-compose.template.yml, and README.md to replace USER_MAC with USER_IP for improved clarity and accuracy in network configuration. Update documentation to guide users on entering their actual IP address.

--- cqu_net.sh
+++ cqu_net.sh
@@ -6,7 +6,7 @@ CHECK_INTERVAL=${CHECK_INTERVAL:-60}
 while true; do
     if ! ping -c 1 www.baidu.com > /dev/null 2>&1; then
         echo "网络断开，尝试重新登录..."
-        curl "http://10.254.7.4:801/eportal/portal/login?callback=dr1004&login_method=1&user_account=%2C0%2C${USER_ACCOUNT}&user_password=${USER_PASSWORD}&wlan_user_ip=10.235.69.41&wlan_user_ipv6=&wlan_user_mac=${USER_MAC}&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.2&terminal_type=1&lang=zh-cn&v=2190&lang=zh" \
+        curl "http://10.254.7.4:801/eportal/portal/login?callback=dr1004&login_method=1&user_account=%2C0%2C${USER_ACCOUNT}&user_password=${USER_PASSWORD}&wlan_user_ip=${USER_IP}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.2&terminal_type=1&lang=zh-cn&v=2190&lang=zh" \
             -H 'Accept: */*' \
             -H 'Accept-Language: zh-CN,zh;q=0.9' \
             -H 'Connection: keep-alive' \
